ee37f1af024b0a89a47a72b97e14738d739a027a
HST <huangshitong0314@163.com>
2026-08-25 15:51:05 +0800

    v0.0.1

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import uvicorn
+from configs.env_loader import load_dotenv
 
-
+load_dotenv(".env")
 def main() -> None:
     uvicorn.run("main_front:app", host="127.0.0.1", port=8000, reload=False)
 
